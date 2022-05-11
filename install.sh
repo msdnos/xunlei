46df21e264272fde67d340199cee4b87b67434f7
msdnos <58739862+msdnos@users.noreply.github.com>
2022-05-11 22:07:31 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 set -eu
 
 latest=v2.6.1
-#latest=$(curl -fsSL https://api.github.com/repos/cnk3x/xunlei/releases/latest | grep browser_download_url | grep $(uname -m) | head -n 1 | grep -Eo https.+.tar.gz)
+#latest=$(curl -fsSL https://api.github.com/repos/msdnos/xunlei/releases/latest | grep browser_download_url | grep $(uname -m) | head -n 1 | grep -Eo https.+.tar.gz)
 latest=https://down.9930.top:4466/api/xunlei/xunlei-v2.6.1.linux.x86_64.tar.gz
 echo "download: $latest"
 curl -fsSL ${latest} | tar zx
